fe797e31eb6934a18854e8f006e24e121e3f3277
Rvz4 <soderlundjs22@gcc.edu>
2025-02-17 15:14:33 -0500

    added print method

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        Print("Hello Git!");
+    }
+
+    public static void Print(String s) {
+        System.out.println(s);
+    }
 }
