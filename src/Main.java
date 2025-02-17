fa439195a57553be6e3754349b5dcad729277616
Rvz4 <soderlundjs22@gcc.edu>
2025-02-17 15:37:49 -0500

    Added gitignore

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Print("Hello Master!");
         Print("System.out.println -> Print");
+        Print("Dev ops stuff");
     }
 
     public static void Print(String s) {
