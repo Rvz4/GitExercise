54ee8b29d1a71ee3aaf49f21bb887a3e31a70b27
Rvz4 <soderlundjs22@gcc.edu>
2025-02-17 15:24:53 -0500

    System.out.println -> Print :)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Print("Hello Master!");
+        Print("System.out.println -> Print");
     }
 
     public static void Print(String s) {
